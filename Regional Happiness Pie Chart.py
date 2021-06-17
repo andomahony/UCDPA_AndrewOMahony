f4c5af8ddcf696ba90299e64d7a2e169c21ff02e
andomahony <anom@hiddenhearing.ie>
2021-06-17 20:25:32 +0100

    17th JUne

--- Regional Happiness Pie Chart.py
+++ Regional Happiness Pie Chart.py
@@ -19,17 +19,17 @@ WHR05_20 = WHR05_20.rename(columns={'Life Ladder': 'Ladder score'})
 WHR05_20 = WHR05_20.dropna()
 WHR05_20.reset_index(inplace=True)
 
-
+# Sorting and defining Regional Mean
 WHR_Reg_Mean = WHR05_20.groupby("Regional indicator")["Ladder score"].mean()
 WHR_Reg_Mean = pd.DataFrame(WHR_Reg_Mean)
 WHR_Reg_Mean.reset_index(inplace=True)
-print(WHR_Reg_Mean)
 
 # Defining absolute
 def pie_values(mean_values):
     display_values  = np.round(mean_values/100.*WHR_Reg_Mean["Ladder score"].sum(), 2)
     return display_values
 
+# Plotting pie chart to display mean by region
 plt.pie(WHR_Reg_Mean["Ladder score"], labels=WHR_Reg_Mean["Regional indicator"], startangle=90, autopct=pie_values, explode =(0,0,0,0,0,0.2,0,0,0,0.1), shadow= True)
 plt.title("Happiness by regional area")
 plt.show()
