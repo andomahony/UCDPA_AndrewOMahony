5542825c4668424d97d2fef697f032e671e36897
andomahony <anom@hiddenhearing.ie>
2021-06-18 13:28:28 +0100

    18th June

--- Regional Happiness Pie Chart.py
+++ Regional Happiness Pie Chart.py
@@ -30,7 +30,7 @@ def pie_values(mean_values):
     return display_values
 
 # Plotting pie chart to display mean by region
-plt.pie(WHR_Reg_Mean["Ladder score"], labels=WHR_Reg_Mean["Regional indicator"], startangle=90, autopct=pie_values, explode =(0,0,0,0,0,0.2,0,0,0,0.1), shadow= True)
+plt.pie(WHR_Reg_Mean["Ladder score"], labels=WHR_Reg_Mean["Regional indicator"], startangle=90, autopct=pie_values, explode =(0,0,0,0,0,0.2,0,0,0,0.1), shadow= True, textprops={'fontsize': 12})
 plt.title("Happiness by regional area")
 plt.show()
 
